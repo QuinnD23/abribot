146b404d8a1516acacf54572de7a28be076d8cb6
QuinnD23 <danilquinn21@gmail.com>
2021-09-18 22:54:53 +0300

    1

---
+++ kyeboards/inline/in_buttons.py
@@ -0,0 +1,21 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+from kyeboards.inline.in_datas import check_callback
+
+InlineForm18 = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Написать❤️", url="https://t.me/watchedtr"),
+        ],
+    ],
+    resize_keyboard=True
+)
+
+InlineForm = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Написать❤️", url="https://t.me/wathed"),
+        ],
+    ],
+    resize_keyboard=True
+)
